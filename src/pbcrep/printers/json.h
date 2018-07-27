ba18412c70787195fc3cda32f98f9e9261b8603e
Dave Benson <daveb@ffem.org>
2018-07-26 23:44:58 -0700

    progress

---
+++ src/pbcrep/printers/json.h
@@ -0,0 +1,16 @@
+
+typedef struct PBCREP_JSON_PrinterOptions PBCREP_JSON_PrinterOptions;
+struct PBCREP_JSON_PrinterOptions
+{
+  unsigned quoteless_keys : 1;
+  unsigned formatted : 1;
+
+  // if formatted
+  unsigned indent_size;
+};
+
+PBCREP_Printer *pbcrep_printer_new_json (ProtobufCMessageDescriptor *desc,
+                                         PBCREP_JSON_PrinterOptions *options,
+                                         PBCREP_PrinterTarget        target);
+
+
---
+++ src/pbcrep/printers/length-prefixed.h
@@ -0,0 +1,6 @@
+
+PBCREP_Printer *pbcrep_printer_new_length_prefixed
+                                 (PBCREP_LengthPrefixed_Format lp_format,
+                                  const ProtobufCMessageDescriptor *desc,
+                                  PBCREP_PrinterTarget       target);
+
